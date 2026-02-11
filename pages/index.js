e79debfd2be56604116cbf820b0030ba127ae9bc
1777marcos <1777santos@gmail.com>
2026-02-11 18:25:57 -0300

    1ºParte

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+function App() {
+  return (
+    <>
+      <h1>Corporação Luto!</h1>
+
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+        </ul>
+      </nav>
+
+      <main>
+        <section>
+          <h2>Departamento de contabilidade:</h2>
+          <p>Lucros em %</p>
+        </section>
+
+        <section>
+          <h3> Departamento de administração:</h3>
+          <p>TI</p>
+          <p>Garagem</p>
+        </section>
+      </main>
+
+      <footer>© 2026 Companhia Luto!</footer>
+    </>
+  );
+}
+
+export default App;
